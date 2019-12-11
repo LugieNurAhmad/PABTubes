23e2509aea1101c5ebf7130daf6638b8f5115949
4-m <empatm@bitbucket.org>
2019-12-11 21:30:27 +0700

    tambah bottom Navigationbar java(skizo,penanganan,alarmreminder)

--- AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/SkizoActivity.java
+++ AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/SkizoActivity.java
@@ -1,8 +1,14 @@
 package com.example.pabtubes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pabtubes.reminder.AlarmReminder;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SkizoActivity extends AppCompatActivity {
 
@@ -10,5 +16,31 @@ public class SkizoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skizo);
+
+        //code untuk Bottom Navigation Bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.about);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.about:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.reminder:
+                        startActivity(new Intent(getApplicationContext(), AlarmReminder.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profil:
+                        startActivity(new Intent(getApplicationContext(),home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
