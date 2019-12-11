566b443d8339b43e2c9217b59125adf2b9a251b6
4-m <empatm@bitbucket.org>
2019-12-11 19:55:51 +0700

    tambah notifikasi reminder channel utk Oreo keatas

--- AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/reminder/AlarmReceiver.java
+++ AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/reminder/AlarmReceiver.java
@@ -1,11 +1,13 @@
 package com.example.pabtubes.reminder;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.example.pabtubes.R;
 
@@ -14,6 +16,7 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         int notificationId = intent.getIntExtra("notificationId",0);
         String message = intent.getStringExtra("todo");
+        int importance =NotificationManager.IMPORTANCE_HIGH;
 
         Intent mainIntent = new Intent(context, AlarmReminder.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,mainIntent,0);
@@ -27,6 +30,15 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
                 .setContentIntent(contentIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelId = "Reminder_id";
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "Reminder",
+                    importance);
+            myNotificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);        }
 
         myNotificationManager.notify(notificationId, builder.build());
     }
