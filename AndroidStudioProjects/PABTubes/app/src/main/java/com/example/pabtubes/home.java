7d6e6a1ffba575a935a7bdf86fda02f3198a7239
4-m <empatm@bitbucket.org>
2019-12-11 19:38:36 +0700

    pesan cek

--- AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/home.java
+++ AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/home.java
@@ -7,6 +7,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+//cek
 public class home extends AppCompatActivity implements View.OnClickListener {
     private CardView tesCard, infoCard, skizoCard, penangananCard;
 
