ca6e3e8bbefa9586eb2b1a1f31be04a1400a7eee
Lugie <ahmadlugie33@gmail.com>
2019-12-12 19:31:36 +0700

    profil dan test kosong

---
+++ AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/tes_paranoid.java
@@ -0,0 +1,14 @@
+package com.example.pabtubes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tes_paranoid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tes_paranoid);
+    }
+}
