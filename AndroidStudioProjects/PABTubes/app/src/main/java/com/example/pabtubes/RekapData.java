cbe1dfde037471380d809ba9e8ecea0d0c1cd5ea
4-m <empatm@bitbucket.org>
2019-12-16 00:24:59 +0700

    add db ulang

--- AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/RekapData.java
+++ AndroidStudioProjects/PABTubes/app/src/main/java/com/example/pabtubes/RekapData.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
 public class RekapData extends AppCompatActivity {
 
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rekap_data);
+
+//        db.collection("data_penyakit").get()
+//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//                        if (!queryDocumentSnapshots.isEmpty()){
+//                            List<DocumentSnapshot> list =queryDocumentSnapshots.getDocuments();
+//                            for (DocumentSnapshot d :list){
+//
+//                            }
+//                        }
+//                    }
+//                });
     }
 }
